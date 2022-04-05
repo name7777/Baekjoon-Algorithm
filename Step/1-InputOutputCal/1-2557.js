872bdd5e8c192d2e0d4ce4c3c08d028bb35944d1
KWON WONHYUN <mywonhyuni@gmail.com>
2022-04-05 23:28:51 +0900

    220405

---
+++ Step/1-InputOutputCal/1-2557.js
@@ -0,0 +1,7 @@
+// Hello World!를 출력하라
+
+function myFunc() {
+    console.log("Hello World!");
+};
+
+myFunc();
---
+++ Step/1-InputOutputCal/2-10718.js
@@ -0,0 +1,8 @@
+// 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+
+function myFunc() {
+    console.log("강한친구 대한육군");
+    console.log("강한친구 대한육군");
+};
+
+myFunc();
